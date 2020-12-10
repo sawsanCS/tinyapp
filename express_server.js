e781fbb9ae024fddccc333f918e55ab8876de217
sawsanCS <sawsan_sli3i@live.fr>
2020-12-10 18:07:43 +0000

    first post : posting a new url

--- express_server.js
+++ express_server.js
@@ -1,5 +1,5 @@
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+
 
 const express = require("express");
 const app = express();
@@ -7,12 +7,17 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+//adding a helper function to generate a random string
+function generateRandomString() {
+  return Math.random().toString(36).substr(2,8);
+}
+
 // adding a route to the new url template
 app.get("/urls/new", (req, res) => {
     res.render("urls_new");
@@ -37,7 +42,14 @@ app.get('/urls/:shortURL', (req, res) => {
     const templateVars = {shortURL: shortURL, longURL: urlDatabase[shortURL]};
     res.render('urls_show', templateVars);
 
-});  
+});
+//addding our first post request to send the url to the list of urls
+app.post('/urls', (req, res) => {
+    longURL = req.body.longURL;
+    shortURL = generateRandomString();
+    urlDatabase[shortURL]= longURL;
+    res.redirect('/urls');
+})  
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
