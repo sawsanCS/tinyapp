e8c96b51b18d9c8488c570df39e080b6d643dd33
sawsanCS <sawsan_sli3i@live.fr>
2020-12-10 17:16:38 +0000

    added a new route to display a single url

--- express_server.js
+++ express_server.js
@@ -14,12 +14,23 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+// adding a new route to urls 
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+  });
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
   app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
   });
+  // adding a new route to display a single route
+app.get('/urls/:shortURL', (req, res) => {
+    const templateVars = {url : urlDatabase[shortURL]};
+    res.render('urls_show', templateVars);
+
+})  
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
