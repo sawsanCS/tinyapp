c12b7d23cee2f636d0736f09f56eaa9377240fa0
sawsanCS <sawsan_sli3i@live.fr>
2020-12-13 00:35:32 +0000

    implemented helpers.js for helper functions

---
+++ helpers.js
@@ -0,0 +1,9 @@
+//adding a helper function to fetch if a user exists by his email
+const getUserByEmail = function (email, listUsers) {
+    for (const u in listUsers) {
+      if (listUsers[u].email === email) {
+        return listUsers[u];
+      }
+    }
+    return null;
+  }
